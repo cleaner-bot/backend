5444583550471c3644d56a14b529b7c523cedd09
Leo Developer <leodeveloper@protonmail.com>
2022-04-12 10:41:32 +0200

    load env_backend too

--- cleaner_back/__init__.py
+++ cleaner_back/__init__.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv(Path("~/.cleaner/secrets").expanduser())
 load_dotenv(Path("~/.cleaner/env").expanduser())
+load_dotenv(Path("~/.cleaner/env_backend").expanduser())
 
 from .main import app  # noqa: E402
 
