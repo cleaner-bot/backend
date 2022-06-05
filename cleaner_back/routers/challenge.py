59271a1f22860944c108f33ecef75739c1ab8ab1
Leo Developer <leodeveloper@protonmail.com>
2022-06-05 18:27:47 +0200

    login or captcha

--- cleaner_back/routers/challenge.py
+++ cleaner_back/routers/challenge.py
@@ -59,7 +59,7 @@ async def get_challenge(
 async def post_challenge(
     flow: str,
     body: ChallengerRequest | None = None,
-    auth_user_id: str = Depends(with_auth),
+    auth_user_id: str = Depends(with_optional_auth),
     database: Redis[bytes] = Depends(with_database),
 ) -> None:
     if len(flow) != 64 or not all(x in "0123456789abcdef" for x in flow):
@@ -78,8 +78,11 @@ async def post_challenge(
         await database.delete((f"challenge:flow:{flow}",))
         raise HTTPException(400, "Guild does not have interactive challenges enabled")
 
+    forced_captcha = is_captcha
     if auth_user_id != user_id.decode():
-        raise HTTPException(403, "Wrong user account")
+        if forced_captcha:
+            raise HTTPException(403, "Wrong user account")
+        is_captcha = True
 
     if (body is None or body.token is None) == (is_captcha is not None):
         raise HTTPException(400, "Expected or unexpected captcha token")
