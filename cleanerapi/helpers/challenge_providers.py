0d09603ad79a1e13fa0d9019e483db55fba0930c
Leo Developer <leodeveloper@protonmail.com>
2022-11-24 16:41:06 +0100

    fix index

--- cleanerapi/helpers/challenge_providers.py
+++ cleanerapi/helpers/challenge_providers.py
@@ -50,8 +50,7 @@ def verify_button(token: str) -> bool:
         return False
     secret_bytes = bytes([x ^ 0x86 ^ i for i, x in enumerate(decoded[:8])])
     secret = crc32(secret_bytes)
-    print("button - secret", secret)
-    trust = decoded[9] ^ (secret >> 16) & 0xFF
+    trust = decoded[8] ^ (secret >> 16) & 0xFF
     if trust & 0x0F:
         print("button - click not trusted", trust)
         return False
