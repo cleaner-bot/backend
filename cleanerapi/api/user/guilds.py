e11c20f79183f28bce89b84d3bff820e6a0155a2
Leo Developer <leodeveloper@protonmail.com>
2022-12-11 13:06:46 +0100

    switch to guild flags

--- cleanerapi/api/user/guilds.py
+++ cleanerapi/api/user/guilds.py
@@ -21,7 +21,7 @@ bp = Blueprint("UserGuilds", version=1)
 )
 async def get_user_me_guilds(request: Request, database: Redis[bytes]) -> HTTPResponse:
     guilds = typing.cast(
-        dict[int, dict[str, str | bool]],
+        dict[int, dict[str, str | bool | list[str]]],
         {
             int(x["id"]): x
             for x in await get_user_guilds(request, database)
@@ -33,8 +33,14 @@ async def get_user_me_guilds(request: Request, database: Redis[bytes]) -> HTTPRe
         return text(added_guild_ids["message"], 503)
 
     for guild_id, guild in guilds.items():
-        guild["is_added"] = guild_id in added_guild_ids["data"]
-        guild["is_suspended"] = bool(
-            await get_entitlement_field(database, guild_id, "suspended")
-        )
+        guild["flags"] = guild_flags = typing.cast(list[str], [])
+        if guild_id in added_guild_ids["data"]:
+            guild_flags.append("added")
+
+        if await get_entitlement_field(database, guild_id, "suspended"):
+            guild_flags.append("suspended")
+
+        if not guild_flags and await database.exists((f"guild:{guild_id}:config",)):
+            guild_flags.append("suggested")
+
     return json(tuple(guilds.values()))
